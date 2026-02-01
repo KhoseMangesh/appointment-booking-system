7360077b8d519135659d4691de654ad8d7cdc25f
KhoseMangesh <khosemangesh794@gmail.com>
2026-02-02 01:49:09 +0530

    Delete app directory

--- app/api/users/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/mongodb";
-import User from "d:/git/demo/appointment-booking/models/user";
-
-export async function GET() {
-  await connectDB();
-  const users = await User.find();
-  return NextResponse.json(users);
-}
